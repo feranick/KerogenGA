55666cff7be439a785172fdc6dd94df3e5c6d9da
Nicola Ferralis <feranick@hotmail.com>
2016-06-03 13:22:00 -0400

    ga_xyz script now converts to molecular concentrations
    From coefficients of linear combinations.

--- Scripts/ga_xyz.sh
+++ Scripts/ga_xyz.sh
@@ -2,7 +2,7 @@
 #
 # Extract XYZ coordinate file from Kerogen-genome search
 #
-# version 1.2-20150627a
+# version 1.3-20160603
 #
 # Nicola Ferralis <ferralis@mit.edu>
 #
@@ -11,6 +11,7 @@
 
 filename="bestIndividual.txt"
 database="shale"
+outfilePC="bestIndividual_concentrations.csv"
 
 num ()
 {
@@ -22,27 +23,37 @@ num ()
     fi
 }
 
+totconc=0
+while read -r line
+do
+    coeff=$(echo $line | sed 's/.*: //')
+    totconc=$(echo "$totconc+$coeff" | bc)
+
+done <  <(tr -d '\r' < "$filename")
+
+echo
+
 while read -r line
 do
     name=$(echo $line | sed 's/ :.*$//')
-    fitness=$(echo $line | sed 's/.*: //')
-
+    coeff=$(echo $line | sed 's/.*: //')
+    conc=$(echo "scale=2; 100*$coeff/$totconc" | bc)
+    
     echo "Molecule: "  $name
-    echo " Fitness: " $fitness
+    echo " Linear combination coefficient: " $coeff
+    echo " Concentration (%): " $conc
 
     molinfo=$(echo "db.molecule.find({name:\"$name\"})" | mongo $database)
-    #echo $molinfo
+
+    numrings=$(echo $molinfo | sed 's/, \"raman.*$//' | sed 's/.*\"numberofrings\" ://')
+    echo " Number of aromatic rings: " $numrings
 
     molstructname=$(echo $molinfo | sed 's/\" }.*$//' | sed 's/.*\"structurename\" : \"//')
     echo " Molecular structurename: " $molstructname
 
     outfile=$molstructname".xyz"
 
-    numrings=$(echo $molinfo | sed 's/, \"raman.*$//' | sed 's/.*\"numberofrings\" ://')
-    echo " Number of aromatic rings: " $numrings
-
     molstructure=$(echo "db.structure.find({structurename:\"$molstructname\"})" | mongo $database)
-    #echo $molstructure
 
     numberatoms=$(echo $molstructure | grep -o "index" | wc -l)
     echo $numberatoms >> $outfile
@@ -93,7 +104,11 @@ do
 
     done
 
-    echo "XYZ coordinates saved in: " $outfile
-    echo
+    echo " XYZ coordinates saved in: " $outfile
+    echo $name","$conc >> $outfilePC
+    echo 
 
 done < "$filename"
+
+    echo " Concentrations saved in: " $outfilePC
+    echo
