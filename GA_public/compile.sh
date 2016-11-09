528b0b063db5b3e3f392affb4fdf221c13be0fc1
Nicola Ferralis <feranick@hotmail.com>
2016-11-09 15:13:13 -0500

    Updated Mongo java driver.

--- GA_public/compile.sh
+++ GA_public/compile.sh
@@ -2,7 +2,7 @@
 rm -r bin
 mkdir bin
 javac -d bin -cp lib/\* src/general/*.java src/GeneticAlgorithm/*.java
-cp -r lib/*.jar  bin
+#cp -r lib/*.jar  bin
 cp -r lib/MANIFEST.MF bin
 cd bin
 jar cvfm ../kga.jar MANIFEST.MF *
